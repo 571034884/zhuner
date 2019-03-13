24abc088d514ab30d308660d729b65d8e26ae3a5
Administrator <1271030102@qq.com>
2019-03-13 16:19:14 +0800

    创建通用的对话框

---
+++ translate/src/main/java/com/aibabel/translate/view/CommonDialog.java
@@ -0,0 +1,39 @@
+package com.aibabel.translate.view;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class CommonDialog  extends AlertDialog {
+
+
+
+    protected CommonDialog(Context context) {
+        super(context);
+    }
+
+    protected CommonDialog(Context context, boolean cancelable, OnCancelListener cancelListener) {
+        super(context, cancelable, cancelListener);
+    }
+
+    protected CommonDialog(Context context, int themeResId) {
+        super(context, themeResId);
+    }
+
+
+    public void setContentView(int res){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
