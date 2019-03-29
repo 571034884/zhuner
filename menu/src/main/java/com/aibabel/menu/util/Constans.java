257c7eac64816bce3e3b2498d16006331e63117e
cbb <294609622@qq.com>
2019-03-29 20:05:12 +0800

    版本更新，正式服务器

--- menu/src/main/java/com/aibabel/menu/util/Constans.java
+++ menu/src/main/java/com/aibabel/menu/util/Constans.java
@@ -6,8 +6,8 @@ package com.aibabel.menu.util;
 
 public class Constans {
 
-//    public static String HOST_SERVER = "http://abroad.api.joner.aibabel.cn:7001";//默认使用国外
-    public static String HOST_SERVER = "http://39.107.238.111:7001";//测试服务器
+    public static String HOST_SERVER = "http://abroad.api.joner.aibabel.cn:7001";//默认使用国外
+//    public static String HOST_SERVER = "http://39.107.238.111:7001";//测试服务器
 
     public static final String HOST_SERVER_EN = "http://abroad.api.joner.aibabel.cn:7001";//国外服务器域名
     public static final String HOST_SERVER_CH = "http://api.joner.aibabel.cn:7001";//国内服务器域名
