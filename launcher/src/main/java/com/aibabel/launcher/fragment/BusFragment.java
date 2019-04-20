8cc5a07ce627fbec5adb8228c8f6aaf283a63db1
cbb <294609622@qq.com>
2019-04-20 18:30:57 +0800

    菜单more点击事件，添加H5页面

--- launcher/src/main/java/com/aibabel/launcher/fragment/BusFragment.java
+++ launcher/src/main/java/com/aibabel/launcher/fragment/BusFragment.java
@@ -1,16 +1,32 @@
 package com.aibabel.launcher.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.aibabel.baselibrary.base.BaseFragment;
 import com.aibabel.launcher.R;
+import com.aibabel.launcher.utils.Logs;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by fytworks on 2019/4/18.
  */
 
-public class BusFragment extends BaseFragment{
+public class BusFragment extends BaseFragment implements View.OnClickListener{
+
+    @BindView(R.id.more_map_app)
+    CardView moreMapApp;
+    @BindView(R.id.more_huilv_app)
+    CardView moreHuilvApp;
+    @BindView(R.id.more_discount_app)
+    CardView moreDiscountApp;
 
     @Override
     public int getLayout() {
@@ -19,6 +35,32 @@ public class BusFragment extends BaseFragment{
 
     @Override
     public void init(View view, Bundle savedInstanceState) {
+        moreMapApp.setOnClickListener(this);
+        moreHuilvApp.setOnClickListener(this);
+        moreDiscountApp.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.more_map_app:
+                launcherApp("com.aibabel.map");
+                break;
+            case R.id.more_huilv_app:
+                launcherApp("com.aibabel.currencyconversion");
+                break;
+            case R.id.more_discount_app:
+                launcherApp("com.aibabel.coupon");
+                break;
+        }
+    }
+
+    public void launcherApp(String packageStr) {
+        try {
+            Intent LaunchIntent = mContext.getPackageManager().getLaunchIntentForPackage(packageStr);
+            startActivity(LaunchIntent);
+        } catch (Exception e) {
+            Logs.e(packageStr + ":" + e.toString());
+        }
     }
 }
--- launcher/src/main/java/com/aibabel/launcher/fragment/TravelFragment.java
+++ launcher/src/main/java/com/aibabel/launcher/fragment/TravelFragment.java
@@ -2,17 +2,33 @@ package com.aibabel.launcher.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.aibabel.baselibrary.base.BaseFragment;
 import com.aibabel.launcher.R;
 import com.aibabel.launcher.utils.Logs;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by fytworks on 2019/4/18.
  */
 
-public class TravelFragment extends BaseFragment{
+public class TravelFragment extends BaseFragment implements View.OnClickListener{
+
+    @BindView(R.id.more_travel_app)
+    CardView moreTravelApp;
+    @BindView(R.id.more_scenic_app)
+    CardView moreScenicApp;
+    @BindView(R.id.more_country_app)
+    CardView moreCountryApp;
+    @BindView(R.id.more_food_app)
+    CardView moreFoodApp;
 
     @Override
     public int getLayout() {
@@ -21,11 +37,23 @@ public class TravelFragment extends BaseFragment{
 
     @Override
     public void init(View view, Bundle savedInstanceState) {
-
+        moreTravelApp.setOnClickListener(this);
+        moreScenicApp.setOnClickListener(this);
+        moreCountryApp.setOnClickListener(this);
+        moreFoodApp.setOnClickListener(this);
+    }
+    public void launcherApp(String packageStr) {
+        try {
+            Intent LaunchIntent = mContext.getPackageManager().getLaunchIntentForPackage(packageStr);
+            startActivity(LaunchIntent);
+        } catch (Exception e) {
+            Logs.e(packageStr + ":" + e.toString());
+        }
     }
 
-    public void onClick(View view){
-        switch (view.getId()){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.more_travel_app:
                 launcherApp("com.aibabel.fyt_play");
                 break;
@@ -39,16 +67,5 @@ public class TravelFragment extends BaseFragment{
                 launcherApp("com.aibabel.food");
                 break;
         }
-
     }
-
-    public void launcherApp(String packageStr) {
-        try {
-            Intent LaunchIntent = mContext.getPackageManager().getLaunchIntentForPackage(packageStr);
-            startActivity(LaunchIntent);
-        } catch (Exception e) {
-            Logs.e(packageStr + ":" + e.toString());
-        }
-    }
-
 }
