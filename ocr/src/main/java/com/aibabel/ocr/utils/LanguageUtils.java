b2cccffc62ee72a61bf657d670739ab4009b7c1b
Administrator <1271030102@qq.com>
2019-04-22 17:22:40 +0800

    拍照翻译修改 自动选择语言

--- ocr/src/main/java/com/aibabel/ocr/utils/LanguageUtils.java
+++ ocr/src/main/java/com/aibabel/ocr/utils/LanguageUtils.java
@@ -10,8 +10,10 @@ import com.aibabel.ocr.bean.LanBean;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 /**
  * =====================================================================
@@ -260,5 +262,16 @@ public class LanguageUtils {
 
         return language;
     }
+
+
+    public static  Map<String,String> getMap(){
+        Map<String,String> map = new HashMap<>();
+        return map;
+
+
+    }
+
+
+
 
 }
