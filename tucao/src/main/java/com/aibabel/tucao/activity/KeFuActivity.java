371124278a3810a68406846d682569327e4013f3
liuwei <meng_liuwei@163.com>
2019-04-25 09:49:03 +0800

    数据统计建立跨进程通信

--- tucao/src/main/java/com/aibabel/tucao/activity/KeFuActivity.java
+++ tucao/src/main/java/com/aibabel/tucao/activity/KeFuActivity.java
@@ -16,7 +16,7 @@ public class KeFuActivity extends BaseActivity {
     ImageView ivClose;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ke_fu);
         ButterKnife.bind(this);
