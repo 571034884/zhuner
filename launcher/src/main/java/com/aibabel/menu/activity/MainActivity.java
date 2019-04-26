f96c3ecd7078084ff9e4e2ecb82250897006ac03
houjunshuai <houjunshuai@aibabel.com>
2019-04-26 16:20:34 +0800

    修正刚才提交，服务器地址为正式服务器

--- launcher/src/main/java/com/aibabel/menu/activity/MainActivity.java
+++ launcher/src/main/java/com/aibabel/menu/activity/MainActivity.java
@@ -1161,8 +1161,9 @@ public class MainActivity extends LaunBaseActivity implements NetBroadcastReceiv
     /**
      * 测试IP
      */
-    private static final String url_sync_order = "https://wx.aibabel.com:3002/common/api/machine/syncOrder";
-//    private static final String url_sync_order = "https://api.web.aibabel.cn:7001/common/api/machine/syncOrder";
+//    private static final String url_sync_order = "https://wx.aibabel.com:3002/common/api/machine/syncOrder";
+   ///这个是正式的
+    private static final String url_sync_order = "https://api.web.aibabel.cn:7001/common/api/machine/syncOrder";
 
     /**
      * 同步订单， hjs
