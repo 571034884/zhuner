220f5727477179f1a174c66a32fef59b8c625284
houjunshuai <houjunshuai@aibabel.com>
2019-04-26 10:18:33 +0800

    从menu移植解锁、SIM卡检测接收者

---
+++ launcher/src/main/java/com/aibabel/message/receiver/MesManageReceiver.java
@@ -0,0 +1,35 @@
+package com.aibabel.message.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Message;
+
+import com.aibabel.menu.activity.MainActivity;
+import com.aibabel.menu.utils.LogUtil;
+
+
+public class MesManageReceiver extends BroadcastReceiver {
+
+    private final static String ACTION_unlock_ok = "com.android.ok";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        final String action = intent.getAction();
+        LogUtil.e("hjs", "Action:" + action);
+        try {
+            if (intent.getAction().equals(ACTION_unlock_ok)) {
+                Message mes= new Message();
+                mes.what = 400;
+                mes.setData(intent.getExtras());
+
+                if (MainActivity.loopHandler != null) {
+                    MainActivity.loopHandler.sendMessage(mes);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
